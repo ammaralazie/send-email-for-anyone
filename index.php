3ac87b4a306e395fb0abeffd7d4672f2fda2d424
ammaralazii <alaziiammar@gmail.com>
2021-04-12 17:21:32 +0300

    add css for project

--- index.php
+++ index.php
@@ -5,39 +5,45 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
     <title>Send Email</title>
 </head>
 
 <body>
-    <form action="" method="post" enctype="multipart/form-data">
-        <input type="text" name="subject" id="" placeholder="subject">
-        <textarea name="message" id="" cols="30" rows="10" placeholder="message"></textarea>
-        <input type="file" name="file" id="" placeholder="file">
-        <input type="email" name="recevire" id="" placeholder="recevire">
-        <button type="submit" name="send">Send Message</button>
-    </form>
-    <?php require_once 'mail.php';
-    if(isset($_POST['send'])){
-        $subject=$_POST['subject'];
-        $email=$_POST['recevire'];
-        $message=$_POST['message'];
-        move_uploaded_file($_FILES['file']['tmp_name'],$_FILES['file']['name']);
-        $fie=$_FILES['file']['name'];
+    <div class="container-form">
+        <div class="box-form">
+            <form action="" method="post" enctype="multipart/form-data">
+                <input type="text" name="subject" id="" placeholder="subject">
+                <textarea name="message" id="" cols="30" rows="10" placeholder="message"></textarea>
+                <input type="file" name="file" id="" placeholder="file">
+                <input type="email" name="recevire" id="" placeholder="recevire">
+                <button type="submit" name="send">Send Message</button>
+            </form>
+        </div>
+    </div>
 
-        $mail->setFrom('alaziiammar@example.com', 'Some One');//this is email will send the message for recevire
+    <?php require_once 'mail.php';
+    if (isset($_POST['send'])) {
+        $subject = $_POST['subject'];
+        $email = $_POST['recevire'];
+        $message = $_POST['message'];
+        move_uploaded_file($_FILES['file']['tmp_name'], $_FILES['file']['name']);
+        $fie = $_FILES['file']['name'];
+
+        $mail->setFrom('alaziiammar@example.com', 'Some One'); //this is email will send the message for recevire
         $mail->addAddress($email);
-    
+
         #الرسالة تتكون من قسمين الموضوع و المحتوى
         $mail->Subject =  $subject;
-    
+
         #المحتوى ممكن يكون رسالة عادية او قالب تم انشائه عن طريق الج تي ام ال وكذلك سي  اس اس
-        $mail->Body= strip_tags($message);
-        if (!empty($fie)){
+        $mail->Body = strip_tags($message);
+        if (!empty($fie)) {
             $mail->addAttachment($fie);
         }
-            //send file to recevire
+        //send file to recevire
         $mail->send();
-    }//end if 
+    } //end if 
     ?>
 </body>
 
--- mail.php
+++ mail.php
@@ -11,7 +11,7 @@ require 'mailer/autoload.php';
 //Instantiation and passing `true` enables exceptions
 $mail = new PHPMailer(true);
 //يستخدم هذا الجزء لعؤض اخطاء في كان هناك اخطاء في عمليه ارسال الرسالة 
-$mail->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose debug output
+//$mail->SMTPDebug = SMTP::DEBUG_SERVER;                      //only in developmet time
 $mail->isSMTP();                                            //Send using SMTP
 $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
 $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
