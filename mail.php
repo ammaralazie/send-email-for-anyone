e11f65f8fc2a3e9e7ca78291c3f8c22f7ee02db0
ammaralazii <alaziiammar@gmail.com>
2021-04-12 17:38:01 +0300

    update on mail-page

--- mail.php
+++ mail.php
@@ -1,6 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="" method="post">
+        <input type="email" name="youremail" placeholder="Enter your email" id="">
+        <input type="text" name="password" placeholder="Password for email" id="">
+        <button type="submit" name="login">Login</button>
+    </form>
+</body>
+</html>
+<?php
+session_start();;
+if (isset($_POST['login'])){
+    $_SESSION['email']=$_POST['youremail'];
+    $password=$_POST['password'];
+}
+?>
 <?php
 //Import PHPMailer classes into the global namespace
 //These must be at the top of your script, not inside a function
+
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\SMTP;
 use PHPMailer\PHPMailer\Exception;
@@ -15,8 +39,8 @@ $mail = new PHPMailer(true);
 $mail->isSMTP();                                            //Send using SMTP
 $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
 $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
-$mail->Username   = 'alaziiammar@gmail.com';                     //SMTP username
-$mail->Password   = 'Iloveyoushahad1';                               //SMTP password
+$mail->Username   =  $_SESSION['email'];                     //SMTP username
+$mail->Password   = $password;                               //SMTP password
 $mail->SMTPSecure = 'ssl';         //Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
 
 
